7c5e62d330a531c8259db25a3adb3d5734ff3a65
Nandlalverma <91118764+Nandlalverma@users.noreply.github.com>
2024-10-13 13:37:59 +0530

    little changes (#1)

--- src/main/java/com/greadLearing/GreatLearing/p1/Demo.java
+++ src/main/java/com/greadLearing/GreatLearing/p1/Demo.java
@@ -3,8 +3,11 @@ package com.greadLearing.GreatLearing.p1;
 public class Demo {
 
     public static void main(String[] args) {
+        System.out.println("DevClass");
+        // this is updated by peter
         System.out.println("DevClass ");
         System.out.println("hi");
         // checkin Stream2learn
+
     }
 }
