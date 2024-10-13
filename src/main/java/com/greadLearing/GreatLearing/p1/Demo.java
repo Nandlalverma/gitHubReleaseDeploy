2bf68298a0b69141d859f55073f7dd01616be153
Nandlalverma <91118764+Nandlalverma@users.noreply.github.com>
2024-10-13 13:30:51 +0530

    Update Demo.java
    updated

--- src/main/java/com/greadLearing/GreatLearing/p1/Demo.java
+++ src/main/java/com/greadLearing/GreatLearing/p1/Demo.java
@@ -3,7 +3,8 @@ package com.greadLearing.GreatLearing.p1;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("DevClass");
+        System.out.println("DevClass ");
+        System.out.println("hi");
         // checkin Stream2learn
     }
 }
