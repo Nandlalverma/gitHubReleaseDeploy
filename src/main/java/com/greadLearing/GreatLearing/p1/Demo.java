2da307022825a739a9416c6ae9f066e42bbd6af3
hibernate_1 <nandlalverma8009@gmail.com>
2024-10-13 11:47:12 +0530

    Initial commit

---
+++ src/main/java/com/greadLearing/GreatLearing/p1/Demo.java
@@ -0,0 +1,8 @@
+package com.greadLearing.GreatLearing.p1;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("DevClass");
+    }
+}
---
+++ src/test/java/com/greadLearing/GreatLearing/p1Test/demoTest.java
@@ -0,0 +1,7 @@
+package com.greadLearing.GreatLearing.p1Test;
+
+public class demoTest {
+    public static void main(String[] args) {
+        System.out.println("DveClass");
+    }
+}
